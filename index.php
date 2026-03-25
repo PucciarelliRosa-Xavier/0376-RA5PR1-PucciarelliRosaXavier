34daed4a70749ae9c0275597b72666546477c819
PucciarelliRosa-Xavier <xpucciarelli25@ilb.cat>
2026-03-25 11:58:11 +0100

    Bucle per generar la tabla de multiplicar
    Es van iterant els numeros de l'1 al 10. Per cada iteració es calcula el resultat i es generen les files de la taula HTML per mostrar l'operació i el resultat.

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+$numero = 7;
 
+echo "<table>";
+ for ($i = 1; $i <= 10; $i++) {        
+        echo "<tr>";
+        echo "<td>$numero x $i</td>";
+        echo "<td>" . ($numero * $i) . "</td>";
+        echo "</tr>";
+}
+echo "</table>";
+?>
