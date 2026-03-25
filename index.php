8045528d8942cc11f32e9b8521b891065a431feb
PucciarelliRosa-Xavier <xpucciarelli25@ilb.cat>
2026-03-25 12:38:27 +0100

    Estils per la taula
    Afegir una classe en funció de si el numero es parell o senar.

--- index.php
+++ index.php
@@ -5,20 +5,39 @@
 <html lang="en">
 <head>
 	   <title>Taula de multiplicar</title>
-	   <style></style>
+	<style>
+    td {
+        border: 1px solid #000;
+        padding: 20px;       
+        text-align: center;
+        font-size: 18px;    
+    }
+        .pair {
+            background-color: yellow;
+        }
+        .odd {
+            background-color: blue;
+		</style>
+		
 </head>
-
+	
 <body>
 <h1>Taula de mutliplicar </h1>
 <?php
   echo "<table>";
-   for ($i = 1; $i <= 10; $i++) {        
-          echo "<tr>";
+   for ($i = 1; $i <= 10; $i++) {   
+	if ($i % 2 == 0) {
+    	$tipus = "pair";
+		} else {
+    	$tipus = "odd";
+		}
+          echo "<tr class='$tipus'>";
           echo "<td>$numero x $i</td>";
           echo "<td>" . ($numero * $i) . "</td>";
           echo "</tr>";
   }
   echo "</table>";
 ?>
+
 </body>
 </html>
