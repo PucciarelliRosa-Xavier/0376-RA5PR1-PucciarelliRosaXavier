5d2694e72e13381f3b3c06ef9851bb25c435c1b8
PucciarelliRosa-Xavier <xpucciarelli25@ilb.cat>
2026-03-25 12:13:16 +0100

    Afegir codi html
    Creacio de la estructura basica de HTML, d'un titiol H1 i l'etiqueta style per afegir el css en un futur

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
-$numero = 7;
-
-echo "<table>";
- for ($i = 1; $i <= 10; $i++) {        
-        echo "<tr>";
-        echo "<td>$numero x $i</td>";
-        echo "<td>" . ($numero * $i) . "</td>";
-        echo "</tr>";
-}
-echo "</table>";
+	$numero = 7;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	   <title>Taula de multiplicar</title>
+	   <style></style>
+</head>
+
+<body>
+<h1>Taula de mutliplicar </h1>
+<?php
+  echo "<table>";
+   for ($i = 1; $i <= 10; $i++) {        
+          echo "<tr>";
+          echo "<td>$numero x $i</td>";
+          echo "<td>" . ($numero * $i) . "</td>";
+          echo "</tr>";
+  }
+  echo "</table>";
+?>
+</body>
+</html>
